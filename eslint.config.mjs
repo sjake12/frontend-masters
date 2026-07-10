3485401871bebbc9239340aaf24a85084ea582b0
sjake12 <apostol.sjake12@gmail.com>
2026-07-10 22:41:25 +0800

    added prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  prettier,
+];
