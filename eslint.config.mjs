52df8f0622b42df4cd14f6d0b8d775c9f6df533f
sjake12 <apostol.sjake12@gmail.com>
2026-07-11 11:25:22 +0800

    added order pizza component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -15,6 +16,19 @@ export default [
         },
       },
     },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
+    },
   },
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
   prettier,
 ];
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
+import Order from "./Order";
+import Pizza from "./Pizza";
 
+const App = () => {
+  return (
+    <div>
+      <h1>Padre Gino's Pizza – Order Now</h1>
+      {/* <Pizza
+        name="Pepperoni"
+        description="Mozzarella Cheese, Pepperoni"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="The Hawaiian Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/hawaiian.webp"}
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        image={"/public/pizzas/big_meat.webp"}
+      /> */}
+      <Order />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
 const root = createRoot(container);
+root.render(<App />);
